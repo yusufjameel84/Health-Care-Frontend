394ed96f93dd77f23b68a1fae9553a3cc325870d
yusufjameel84 <jameelyusuf0@gmail.com>
2021-08-07 00:31:33 +0530

    bugfix

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom';
 import Routes from './routes/routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.render(<Routes/>, document.getElementById('root'));
+
+console.log("Hey");
